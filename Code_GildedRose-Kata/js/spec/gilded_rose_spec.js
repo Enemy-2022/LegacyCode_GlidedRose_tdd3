b87af9482470c4ee045e52e2e16c809c262c9e1a
mesha <mesha@KATIJA>
2026-06-01 22:56:35 +0200

    R5: sulfurs verändert weder Qualität noch sell_in

--- Code_GildedRose-Kata/js/spec/gilded_rose_spec.js
+++ Code_GildedRose-Kata/js/spec/gilded_rose_spec.js
@@ -27,4 +27,11 @@ describe("Gilded Rose", function() {
     expect(items[0].quality).toEqual(50);
   });
 
+  it("Sulfuras, being a legendary item, never has to be sold or decreases in Quality", function() {
+    items = [ new Item("Sulfuras, Hand of Ragnaros", 0, 80) ];
+    update_quality();
+    expect(items[0].quality).toEqual(80);
+    expect(items[0].sell_in).toEqual(0);
+  });
+
 });
