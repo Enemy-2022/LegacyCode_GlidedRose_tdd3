d51f191805283d919557550cb15d5f95c21aef1a
mesha <mesha@KATIJA>
2026-06-01 23:26:04 +0200

    failing test : conjured items verlieren qualitaet so schnell.

--- Code_GildedRose-Kata/js/spec/gilded_rose_spec.js
+++ Code_GildedRose-Kata/js/spec/gilded_rose_spec.js
@@ -52,4 +52,10 @@ describe("Gilded Rose", function() {
   expect(items[0].quality).toEqual(0);
 });
 
+
+ it("Conjured items degrade in Quality twice as fast as normal items", function() {
+    items = [ new Item("Conjured Mana Cake", 5, 10) ];
+    update_quality();
+    expect(items[0].quality).toEqual(8);
+  });
 });
